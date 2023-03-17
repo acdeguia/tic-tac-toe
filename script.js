369dc7c2eeba39f7572d27a8b00e2816e16ef212
Cate <anncatherinedeguia@gmail.com>
2023-03-17 10:00:41 +0800

    Implement handleClick function for player marks

--- script.js
+++ script.js
@@ -1,72 +1,90 @@
 const Gameboard = (() => {
-    let gameboard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
-    const display = () => {
-        
-        const container = document.querySelector('#container')
-   
-        let board = "";
-        gameboard.forEach((block, index) => {
-            board += `<div class="block" id=block-${index}>${block}</div>`
-          
-        })
-        container.innerHTML = board
-        const blocks = document.querySelectorAll(".block")
-        blocks.forEach((block) => {
-          block.addEventListener("click", Controller.handleClick)
-        })
-    }
-    return {
-        display,
-    }
+  const display = () => {
+    const container = document.querySelector("#container");
 
+    let board = "";
+    gameboard.forEach((block, index) => {
+      board += `<div class="block" id=block-${index}>${block}</div>`;
+    });
+    container.innerHTML = board;
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+      block.addEventListener("click", Controller.handleClick);
+    });
+  };
+
+  const update = (index, value) => {
+    gameboard[index] = value;
+    display();
+  };
+
+  const getGameboard = ()  => gameboard;
+
+  return {
+    display,
+    update,
+    getGameboard,
+  };
 })();
 
 const playerFactory = (name, mark) => {
-    return {
-        name,
-        mark, 
-    }
-}
-
+  return {
+    name,
+    mark,
+  };
+};
 
 const Controller = (() => {
-    let players = []
-    let currentIndex;
-    let gameOver = false
+  let players = [];
+  let currentIndex;
+  let gameOver = false;
 
-    const start = () => {
+  const start = () => {
+    const playerOne = document.querySelector("#player1").value;
+    const playerTwo = document.querySelector("#player2").value;
 
-          const playerOne = document.querySelector('#player1').value
-          const playerTwo = document.querySelector('#player2').value
+    players = [playerFactory(playerOne, "X"), playerFactory(playerTwo, "O")];
+    currentIndex = 0;
+    gameOver = false;
 
-         players = [
-            playerFactory(playerOne, "X"),
-            playerFactory(playerTwo, "O")
-         ]
-         currentIndex = 0
-         gameOver = false;
-         document.querySelector(".p1").innerHTML = playerOne.toUpperCase()
-         document.querySelector(".p2").innerHTML = playerTwo.toUpperCase()
-
-         Gameboard.display()
+    if (playerOne == "") {
+      document.querySelector(".p1").innerHTML = "PLAYER 1";
+    } else {
+      document.querySelector(".p1").innerHTML = playerOne.toUpperCase();
     }
 
-    const handleClick = (event) => {
-      alert('hello')
+    if (playerTwo == "") {
+      document.querySelector(".p2").innerHTML = "PLAYER 2";
+    } else {
+      document.querySelector(".p2").innerHTML = playerTwo.toUpperCase();
     }
+    Gameboard.display();
 
-    return {
-        start,
-        handleClick,
-    }
+    
+  };
 
-})()
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    if(Gameboard.getGameboard() [index] !== "")
+    return
+    
+      Gameboard.update(index, players[currentIndex].mark);
+      currentIndex = currentIndex === 0 ? 1 : 0
+    
+  };
 
-const start = document.querySelector('#start')
+  return {
+    start,
+    handleClick,
+  };
+})();
 
-start.addEventListener('click', () => {
-    Controller.start();
-    document.getElementById('form-container').style.display = `none`;
-    document.getElementById('score').style.display = `contents`;
-})
+const start = document.querySelector("#start");
+
+start.addEventListener("click", () => {
+  Controller.start();
+  document.getElementById("form-container").style.display = `none`;
+  document.getElementById("score").style.display = `contents`;
+});
